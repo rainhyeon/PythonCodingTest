8f896dfeca500f6175efa20ac5ee800223e01d94
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-08-10 11:36:10 +0900

    [level 3] Title: 등굣길, Time: 3.50 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,25 @@
+def solution(m, n, puddles):
+    answer = 0
+    MOD = 1000000007
+    
+    water = set((y-1, x-1) for x, y in puddles)
+    
+    road = [[0] * m for _ in range(n)] # 행 * 열
+    
+    road[0][0] = 1
+    
+    for i in range(n): # 행
+        for j in range(m): # 열
+            if (i, j) in water:
+                road[i][j] = 0
+                continue
+            
+            if i > 0:
+                road[i][j] += road[i-1][j]
+                
+            if j > 0:
+                road[i][j] += road[i][j-1]
+            
+            road[i][j] %= MOD
+            
+    return road[i][j]
