8e37dbf5e508f051cb1e72a74436eaf68cada586
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-08-10 11:06:39 +0900

    [level 4] Title: 징검다리, Time: 156.40 ms, Memory: 11.1 MB -BaekjoonHub

--- Python3/프로그래머스/4/43236. 징검다리/징검다리.py
+++ Python3/프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,27 +1,28 @@
 def solution(distance, rocks, n):
     answer = 0
-    rocks.append(distance) # 마지막 최종 거리 추가
+    
+    # 거리의 최솟 값 -> mid
+    rocks.append(distance)
+    left = 1
+    right = max(rocks)
     rocks.sort()
     
-    left = 1
-    right = distance
-
-    
     while left <= right:
-        mid = (left + right) // 2 # 최솟값 후보
-        prev = 0  # 이전 rock
-        remove = 0 # 제거된 rock 개수
+        mid = (left + right) // 2
+        
+        remove = 0
+        prev = 0
         
         for rock in rocks:
-            if rock - prev < mid: # 현재꺼 - 이전꺼 < 최소값 후보: 최솟값보다 더 적어지면 안되니까
-                remove += 1 # 제거한다
-            else:               
-                prev = rock # 다음 rock에 대해 계산 
-        
-        if remove <= n:     # 최솟값이 너무 커서 많이 제거하지 않았네
-            left = mid + 1  # 최솟값을 줄이자
-            answer = mid    # 바위 제거 수가 n 이하일때 성공
-        else:               # 최솟값이 너무 작아서 많이 제거했다
-            right = mid -1  # 최솟값을 높이자
-    
+            if rock - prev < mid:
+                remove += 1
+            else:
+                prev = rock
+                
+        if remove > n: # 많이 제거됐다 -> mid를 최솟 값이 너무 작았다
+            right = mid - 1
+        else:
+            answer = mid
+            left = mid + 1
+                
     return answer
