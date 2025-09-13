986a24ac5a25f112ae33bdb048386938940a0be7
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-09-14 01:13:52 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/14501. 퇴사/퇴사.py
+++ Python/백준/Silver/14501. 퇴사/퇴사.py
@@ -3,25 +3,26 @@
 N = int(input()) # 개수
 works = [list(map(int, input().split())) for _ in range(N)]
 #print(works)
-answer = -999999999
 
-def recur(index,price):
+def recur(index):
     global answer
     #print(f"{index}, {price}")
 
     if index == N: # 범위를 넘어가면(N부터)
-        answer = max(answer, price) # 퇴사날 마지막날이후(N)이면 최댓값 계산을 해라
-        return
+        return 0
     if index > N:
-        return
+        return -999999999
+    
+    # 계산되어있다면 
+    if dp[index] != -1:
+        return dp[index] # 계산된것을 가져와라
 
-    # 상담 한다면
-    recur(index+works[index][0], price+works[index][1])
+    # 상답을 받거나 안받거나 중에 더 max인것을 dp에 저장하겠다
+    dp[index] = max(recur(index+works[index][0])+works[index][1], recur(index+1))
+    
+    return dp[index] # 중간에 계산했던걸 가져올 수 있도록 
 
-    # 상담 안한다면
-    recur(index+1, price)
-
-recur(0, 0)
-print(answer)
-
+dp = [-1 for _ in range(N+1)] # 1~시작하도록 하기위해서
+recur(0)
+print(dp[0])
 
