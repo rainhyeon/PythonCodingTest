11ade62cc03d31fad9f020e5fdb82a72e55b76f1
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-10-25 14:14:52 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+
+-- 보호소: Intact, 입양: Spayed
+
+SELECT i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
+FROM ANIMAL_INS i
+JOIN ANIMAL_OUTS o
+    ON i.ANIMAL_ID = o.ANIMAL_ID
+WHERE i.SEX_UPON_INTAKE LIKE 'Intact%' AND
+        (o.SEX_UPON_OUTCOME LIKE 'Spayed%' OR o.SEX_UPON_OUTCOME LIKE 'Neutered%')
+ORDER BY i.ANIMAL_ID
