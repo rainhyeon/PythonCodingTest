fa31e39206e00489ef24229229d0685bd99649e7
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-10-25 13:52:30 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+
+-- 출력: 보호시작 > 입양일
+-- 순서: 보호시작이 작은순
+
+SELECT i.ANIMAL_ID, i.NAME
+FROM ANIMAL_INS i
+JOIN ANIMAL_OUTS o
+    ON i.ANIMAL_ID = o.ANIMAL_ID
+WHERE i.DATETIME > o.DATETIME
+ORDER BY i.DATETIME
