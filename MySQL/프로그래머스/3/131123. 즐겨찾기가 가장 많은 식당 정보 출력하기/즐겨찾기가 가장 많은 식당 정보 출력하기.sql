b7fe8ef3cf483c638698cafd8b7657554c6d5bbf
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-10-29 14:06:56 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ MySQL/프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,16 +1,12 @@
 -- 코드를 입력하세요
--- 식당 정보: REST_INFO
-    -- REST_ID, REST_NAME, FOOD_TYPE, VIEWS, FAVORITES, PARKING_LOT, ADDRESS, TEL
-    -- 식당 ID, 식당 이름, 음식 종류, 조회수, 즐겨찾기수, 주차장 유무, 주소, 전화번호를
-    
--- 출력: 음식 종류별, 즐겨찾기 수가 가장 많은 것
--- 정렬: 음식 종류
+-- 음식 종류별, 즐겨찾기가 가장 많은 식당의 정보
 
-
-SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
-FROM REST_INFO
-WHERE (FOOD_TYPE, FAVORITES) IN (SELECT FOOD_TYPE, MAX(FAVORITES) AS FAVORITES
-                    FROM REST_INFO
-                    GROUP BY FOOD_TYPE)
-ORDER BY FOOD_TYPE DESC
-                    
+WITH max_f AS (SELECT MAX(FAVORITES) AS MAX_FAVORITES, FOOD_TYPE
+                  FROM REST_INFO
+                  GROUP BY FOOD_TYPE)
+SELECT m.FOOD_TYPE, r.REST_ID, r.REST_NAME, r.FAVORITES
+FROM max_f m
+JOIN REST_INFO r
+    ON m.MAX_FAVORITES = r.FAVORITES
+    AND m.FOOD_TYPE = r.FOOD_TYPE
+ORDER BY m.FOOD_TYPE DESC
