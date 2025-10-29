1dacea43c177a99cce329a9e8cbf752c480da925
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-10-29 15:33:09 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ MySQL/프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,22 +1,25 @@
 -- 코드를 작성해주세요
--- 출력: 사원별 성과금 정보 조회
 
-SELECT a.EMP_NO, e.EMP_NAME,
-     CASE
-        WHEN a.SCORE >= 96 THEN 'S'
-        WHEN a.SCORE >= 90 THEN 'A'
-        WHEN a.SCORE >= 80 THEN 'B'
-        ELSE 'C'
-    END AS GRADE,
-    CASE
-        WHEN a.SCORE >= 96 THEN FLOOR(e.SAL * 0.2)
-        WHEN a.SCORE >= 90 THEN FLOOR(e.SAL * 0.15)
-        WHEN a.SCORE >= 80 THEN FLOOR(e.SAL * 0.1)
-        ELSE 0
-    END AS BONUS
-FROM (SELECT EMP_NO, AVG(SCORE) AS SCORE
-    FROM HR_GRADE 
-    GROUP BY EMP_NO) a
-    JOIN HR_EMPLOYEES e
-        ON a.EMP_NO = e.EMP_NO
+-- 직원별 평균 점수
+WITH avg_score AS (SELECT EMP_NO, AVG(SCORE) AS AVG_SCORE,
+                   CASE
+                        WHEN AVG(SCORE) >= 96 THEN 'S'
+                        WHEN AVG(SCORE) >= 90 THEN 'A'
+                        WHEN AVG(SCORE) >= 80 THEN 'B'
+                        ELSE 'C'
+                    END AS GRADE,
+                    CASE
+                        WHEN AVG(SCORE) >= 96 THEN 20
+                        WHEN AVG(SCORE) >= 90 THEN 15
+                        WHEN AVG(SCORE) >= 80 THEN 10
+                        ELSE 0
+                    END AS BONUS
+               FROM HR_GRADE
+               GROUP BY EMP_NO)
+               
+SELECT a.EMP_NO, h.EMP_NAME, a.GRADE,
+    (h.SAL * a.BONUS / 100) AS BONUS
+FROM avg_score a
+JOIN HR_EMPLOYEES h
+    ON a.EMP_NO = h.EMP_NO
 ORDER BY a.EMP_NO
