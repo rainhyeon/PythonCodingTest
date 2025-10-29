4d91a98d2be8207521fd36beb17601d40fe12551
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-10-29 14:46:57 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ MySQL/프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,16 +1,18 @@
 -- 코드를 입력하세요
--- 입양 보낸 정보: ANIMAL_OUTS
-
-WITH RECURSIVE hours AS (
+-- 0시~23시까지, 시간대별 입양 건수
+WITH RECURSIVE hour AS (
     SELECT 0 AS H
     UNION ALL
-    SELECT H+1 FROM hours WHERE H < 23
-)
+    SELECT H+1 FROM hour WHERE H < 23)
+
 
 SELECT 
     h.H AS HOUR,
-    COUNT(a.DATETIME) AS COUNT
-FROM hours h
+    CASE
+        WHEN a.DATETIME IS NULL THEN 0
+        ELSE COUNT(*)
+    END AS COUNT
+FROM hour h
 LEFT JOIN ANIMAL_OUTS a
     ON h.H = HOUR(a.DATETIME)
 GROUP BY h.H
