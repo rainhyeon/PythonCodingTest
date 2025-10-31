41b78b4a0361a7f966ebccd0bffba21eb4e2c0e7
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-10-31 13:15:55 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ MySQL/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,21 +1,17 @@
 -- 코드를 입력하세요
--- 주문 정보: FIRST_HALF 
+-- 상반기 정보: FIRST_HALF
     -- SHIPMENT_ID, FLAVOR, TOTAL_ORDER
-    -- 출하 번호, 아이스크림 맛, 상반기 아이스크림 총주문량
--- 7월 주문 정보: JULY
+    --  출하 번호, 아이스크림 맛, 상반기 아이스크림 총주문량
+    
+-- 7월 정보: JULY
     -- SHIPMENT_ID, FLAVOR, TOTAL_ORDER
     --  출하 번호, 아이스크림 맛, 7월 아이스크림 총주문량
     
--- 외래키: FLAVOR
-
--- 출력: 7월의 총 주문량, 상반기의 총 주문량을 더한 값이 큰 순서대로 상위3개의 맛 조회
-
-SELECT f.FLAVOR
-FROM FIRST_HALF f
-JOIN JULY j
-    ON f.FLAVOR = j.FLAVOR
-GROUP BY f.FLAVOR
-ORDER BY SUM(f.TOTAL_ORDER + j.TOTAL_ORDER) DESC
-LIMIT 3;
-
-
+-- 출력: TOTAL_ORDER + TOTAL_ORDER
+SELECT h.FLAVOR
+FROM FIRST_HALF h
+    JOIN JULY j
+    ON h.FLAVOR = j.FLAVOR
+GROUP BY h.FLAVOR
+ORDER BY (SUM(h.TOTAL_ORDER) + SUM(J.TOTAL_ORDER)) DESC
+LIMIT 3
